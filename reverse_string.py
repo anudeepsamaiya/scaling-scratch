e262b635212ff926c78e44a2e67ad6799b9983cf
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-09-04 11:30:47 +0530

    add recursive reversal

--- reverse_string.py
+++ reverse_string.py
@@ -1,5 +1,13 @@
 s = '#abc#de#eg#'
 
+def recursive_rev(s, temp, index=0):
+    index += 1
+    size = len(s)
+    if index <= size:
+        temp = s[size-index]
+        print(temp)
+        return rev(s, temp, index)
+
 def reverse_str(s):
     ll = []
     for each in s[::-1]:
@@ -8,7 +16,7 @@ def reverse_str(s):
             continue
         temp = [x for x in each[::-1]]
         ll.append(''.join(temp))
-    print ''.join(ll)
+    print(''.join(ll))
 
 def reverse_str2(s):
     ll = [ x[::-1] for x in s[::-1] if x.isalnum() ]
