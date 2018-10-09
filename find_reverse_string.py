15ef4cafe2ba2219f5d7443ece00fd97f5dc246b
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-10-09 14:24:01 +0530

    merge tests and solns and update pytest.ini

--- find_reverse_string.py
+++ find_reverse_string.py
@@ -4,15 +4,29 @@ def rev(s):
     """concatenates inside out"""
     return len(s) != 0 and rev(s[1:]) + s[0] or s
 
+def test_rev():
+    s = '#abc#de#eg#'
+    assert rev(s) == '#ge#ed#cba#'
+
+
 def reverse_and_filter(s):
     """reverse only alnums filtering special characters"""
     if len(s) == 0:
         return s
     return reverse_and_filter(s[1:]) + (s[0].isalnum() and s[0] or '')
 
+def test_reverse_and_filter():
+    s = '#abc#de#eg#'
+    assert reverse_and_filter(s) == 'geedcba'
+
+
 def reverse_only_alnum(s):
     """reverse only alnums leaving special charactes in place"""
     return len(s) != 0 and reverse_only_alnum(s[1:]) + s[0] or s
+
+def test_reverse_only_alnum():
+    s = '#abc#de#eg#'
+    assert reverse_only_alnum(s) == '#gee#dc#ba#'
 
 def reverse_str2(s):
     return ''.join([x[::-1] for x in s[::-1] if x.isalnum()])
