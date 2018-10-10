9fa98bd262f9b5ae320430f04d62e1434936415f
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-10-10 13:32:49 +0530

    define max_arr_sum

--- find_max_contigous_sum_in_an_array.py
+++ find_max_contigous_sum_in_an_array.py
@@ -1,4 +1,16 @@
 import pytest
+
+def find_max_contigous_sum(arr):
+    max_sum = 0
+    for index,_ in enumerate(arr):
+        cur_sum = 0
+        for item in arr[index:]:
+            cur_sum += item
+            max_sum = cur_sum > max_sum and cur_sum or max_sum
+            print("index: ", index, "item:", item, "max: ", max_sum, "cur: ", cur_sum)
+    return max_sum
+
+
 @pytest.fixture(params=[
         ((-2, 1, -3, 4, -1, 2, 1, -5, 4), 6),
         ((-5, 6, 7, 1, 4, -8, 16), 26),
